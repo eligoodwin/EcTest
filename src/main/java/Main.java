d0dc1322a45922b167437dbaa6df81b5f2414e74
eligoodwin <goodwiel@oregonstate.edu>
2017-12-12 17:56:53 -0800

    created instsance

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Main {
+    public static Twitter createInstance(){
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("I4hXsAS3v7ybDN62AAUSosofZ")
+                .setOAuthConsumerSecret("PAvCA4aKXL96k72iHPKLUVgtAKN2KxmPMtgJpVh7emkRFihBBa")
+                .setOAuthAccessToken("911684699218513921-gyTcbKBzd9ZgtxYHVI6cLocrogD438b")
+                .setOAuthAccessTokenSecret("Eo0Rta62aM0QMcciuKc5XhzzvSc22jblSu2Zm4yn02iAu");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+
+        return tf.getInstance();
+    }
+
+
     public static void main(String[] args){
-        System.out.print("Fuck you buddy.");
+        Twitter twitter = createInstance();
+
+
     }
 }
